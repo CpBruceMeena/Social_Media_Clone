4d63b07460dcf54cf047947926d4c10863d54cb9
CpBruceMeena <sandeep10198@gmail.com>
2019-12-14 06:22:02 +0530

    Setting up the two apps, groups and post

--- simplesocial/simplesocial/settings.py
+++ simplesocial/simplesocial/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrap3', 
+    'bootstrap3',
     'accounts',
 ]
 
@@ -121,3 +121,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIR = [os.path.join(BASE_DIR, 'static')]
+
+LOGIN_REDIRECT_URL = 'test'
+LOGOUT_REDIRECT_URL = 'thanks'
